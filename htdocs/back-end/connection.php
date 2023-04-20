204ba1efc478110f7874a7bb557a56861f4db458
mohammedzarhbouch <120816@vistacollege.nl>
2023-04-20 10:43:42 +0200

    nice bush

---
+++ htdocs/back-end/connection.php
@@ -0,0 +1,13 @@
+<?php
+$databaseHost = 'localhost';
+$databaseUser = 'root';
+$databasePassword = '';
+$databaseName = 'challenge10';
+
+$con = mysqli_connect($databaseHost, $databaseUser , $databasePassword , $databaseName);
+if ( mysqli_connect_error() ) {
+	
+	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+
+?>
